f127a08380d650ab57381d7fb82493678fa9b4ae
Lisandro Canessa <lisandrocanessa@gmail.com>
2022-10-31 09:12:48 -0300

    js

---
+++ Clase10/variables.js
@@ -0,0 +1,7 @@
+//variable nos va a permitir guardar informacion en un espacio de memoria y podamos reutilizarlo cada vez que lo necesitemos.
+
+var nombreAlumno = "Juan Perez";
+nombreAlumno = 123;
+
+console.log(typeof(nombreAlumno))
+console.log(nombreAlumno)
