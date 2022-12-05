801cc653d9921ace5b4357ae7c52bfc15594ae90
Lisandro Canessa <lisandrocanessa@gmail.com>
2022-12-05 12:10:18 -0300

    react app

---
+++ Clase19/primera-app/src/components/footer/Footer.js
@@ -0,0 +1,20 @@
+import './Footer.css'
+
+export default function Footer(){
+    return(
+        <footer>
+      <div>
+        <p>
+          Wiki de la saga <span class="fontBLue">Game Of Thrones </span>
+        </p>
+        <p>
+          <span>Lisandro Canessa</span>
+        </p>
+        <p class="col-4 text-center">
+          <span class="fontBLue">Info by</span>
+          <a href="https://hieloyfuego.fandom.com/wiki/Portada">Hielo y Fuego Wiki</a>
+        </p>
+      </div>
+        </footer>
+    )
+}
---
+++ Clase19/primera-app/src/components/nav/Navegacion.js
@@ -0,0 +1,12 @@
+import './Navegacion.css'
+
+export default function Navegacion(props){
+    return(
+        <nav className='row bg-primary'>
+            <ul className='px-5'>
+                <li className='btn btn-danger'>{props.datos.nombre}</li>
+                <li>Sobre Nosotros</li>
+            </ul>
+        </nav>
+    )
+}
